f336a2505db8a30121c59694250c43b31572adba
Hachim Hassani <hachim.hassani@gmail.com>
2025-03-23 14:41:25 +0100

    first commit : mvn clean empty project

---
+++ src/main/java/fr/hachim/quizapi/QuizapiApplication.java
@@ -0,0 +1,13 @@
+package fr.hachim.quizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizapiApplication.class, args);
+	}
+
+}
